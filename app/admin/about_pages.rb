9abd85ce6e627eefcac0ee5382aef96320f38d21
Ramneet Kaur <ramneetkaur4@rrc.ca>
2024-04-19 08:39:16 -0600

    updated about page functionality

--- app/admin/about_pages.rb
+++ app/admin/about_pages.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AboutPage do
-  permit_params :title, :content
+  # permit_params :title, :content
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
