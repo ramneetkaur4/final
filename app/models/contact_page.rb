d7fd04fa6b589d98ae7730efe349f9175d061fbd
Ramneet Kaur <ramneetkaur4@rrc.ca>
2024-04-19 07:49:52 -0600

    added the province ontario in the orders page

--- app/models/contact_page.rb
+++ app/models/contact_page.rb
@@ -1,6 +1,6 @@
 class ContactPage < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
-  # validates :content, presence: true
+  validates :content, presence: true
   def self.ransackable_attributes(auth_object = nil)
     ["content", "created_at", "id", "title", "updated_at"]
   end
